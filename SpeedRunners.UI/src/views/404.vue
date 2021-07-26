b69428e2818400f6987df7a45851f500b2e28872
TinyMaD <845682630@qq.com>
2021-07-26 22:22:14 +0800

    清除eslint提示

--- SpeedRunners.UI/src/views/404.vue
+++ SpeedRunners.UI/src/views/404.vue
@@ -23,13 +23,13 @@
 <script>
 
 export default {
-  name: 'Page404',
+  name: "Page404",
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      return "The webmaster said that you can not enter this page...";
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
